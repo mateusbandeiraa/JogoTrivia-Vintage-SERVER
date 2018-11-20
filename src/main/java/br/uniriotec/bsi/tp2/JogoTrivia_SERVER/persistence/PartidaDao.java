973b1ef27629d6a5f6f7b1239ba7adc67f2f5ca9
mateusbandeiraa <mateusbandeiraa@gmail.com>
2018-11-20 14:27:52 -0200

    Update PartidaDao.java

--- src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_SERVER/persistence/PartidaDao.java
+++ src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_SERVER/persistence/PartidaDao.java
@@ -8,7 +8,7 @@ import javax.persistence.Query;
 import br.uniriotec.bsi.tp2.JogoTrivia_API.EstadoPartida;
 import br.uniriotec.bsi.tp2.JogoTrivia_API.Partida;
 
-public class PartidaDao extends Dao<Partida> implements PartidaDataSource{
+public class PartidaDao extends Dao<Partida> implements PartidaDataSource {
 
 	public PartidaDao() throws DaoException {
 		super(Partida.class);
@@ -16,8 +16,8 @@ public class PartidaDao extends Dao<Partida> implements PartidaDataSource{
 
 	public List<Partida> findByState(EstadoPartida estado) {
 		EntityManager em = emf.createEntityManager();
-		Query q = em.createQuery("FROM Partida WHERE EstadoPartida = :estado");
-		q.setParameter("estado", estado);
+		Query q = em.createQuery("FROM Partida WHERE EstadoAtual = :estado");
+		q.setParameter("estado", estado.name());
 		@SuppressWarnings("unchecked")
 		List<Partida> results = q.getResultList();
 		em.close();
