2e4d7eb634c5089443f7efd6e45c618e93ab4f6e
Mateus Bandeira <mateusbandeiraa@gmail.com>
2018-11-29 17:20:41 -0200

    Novas questoes

--- src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_SERVER/persistence/JogoDummyDao.java
+++ src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_SERVER/persistence/JogoDummyDao.java
@@ -3,12 +3,13 @@ package br.uniriotec.bsi.tp2.JogoTrivia_SERVER.persistence;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.GsonBuilder;
+
 import br.uniriotec.bsi.tp2.JogoTrivia_API.ConjuntoMultiplo;
 import br.uniriotec.bsi.tp2.JogoTrivia_API.ConjuntoOrdenado;
 import br.uniriotec.bsi.tp2.JogoTrivia_API.ConjuntoUnitario;
 import br.uniriotec.bsi.tp2.JogoTrivia_API.Jogo;
 import br.uniriotec.bsi.tp2.JogoTrivia_API.Opcao;
-import br.uniriotec.bsi.tp2.JogoTrivia_API.Partida;
 import br.uniriotec.bsi.tp2.JogoTrivia_API.Questao;
 
 /**
@@ -23,12 +24,12 @@ public class JogoDummyDao implements JogoDataSource {
 	public Jogo find(int id) {
 		Jogo j = new Jogo("Conhecimentos gerais", 0, 1);
 
-		Questao q = new Questao("Qual super herói da Marvel usa um martelo?", 10, 0);
+		Questao q = new Questao("Qual é a atriz conhecida pelo meme das fórmulas matemáticas?", 10, 0);
 
-		Opcao o1 = new Opcao("Homem de Ferro");
-		Opcao o2 = new Opcao("Doutor Estranho");
-		Opcao o3 = new Opcao("Homem-Aranha");
-		Opcao o4 = new Opcao("Thor");
+		Opcao o1 = new Opcao("Renata Sorrah");
+		Opcao o2 = new Opcao("Nazaré Tedesco");
+		Opcao o3 = new Opcao("Regina Duarte");
+		Opcao o4 = new Opcao("Susana Vieira");
 
 		ArrayList<Opcao> opcoes = new ArrayList<>();
 
@@ -40,10 +41,10 @@ public class JogoDummyDao implements JogoDataSource {
 		ConjuntoOrdenado cOpcoes = new ConjuntoOrdenado(opcoes);
 
 		ConjuntoMultiplo cOpcoesARemover = new ConjuntoMultiplo();
-		cOpcoesARemover.adicionarOpcao(o1);
+		cOpcoesARemover.adicionarOpcao(o2);
 		cOpcoesARemover.adicionarOpcao(o3);
 
-		ConjuntoUnitario conjuntoSolucao = new ConjuntoUnitario(o4);
+		ConjuntoUnitario conjuntoSolucao = new ConjuntoUnitario(o1);
 
 		q.setOpcoes(cOpcoes);
 		q.setOpcoesARemover(cOpcoesARemover);
@@ -51,12 +52,12 @@ public class JogoDummyDao implements JogoDataSource {
 
 		j.adicionarQuestao(q);
 
-		q = new Questao("Cada versão do Android recebe um nome relacionado a o que?", 10, 0);
+		q = new Questao("Qual é o nome do grupo conhecido pela música Cheia de Manias?", 10, 0);
 
-		o1 = new Opcao("Animais");
-		o2 = new Opcao("Lugares");
-		o3 = new Opcao("Doces");
-		o4 = new Opcao("Cientistas");
+		o1 = new Opcao("Kid Abelha");
+		o2 = new Opcao("Black Eyed Peas");
+		o3 = new Opcao("Revelação");
+		o4 = new Opcao("Raça Negra");
 
 		opcoes = new ArrayList<>();
 
@@ -68,10 +69,10 @@ public class JogoDummyDao implements JogoDataSource {
 		cOpcoes = new ConjuntoOrdenado(opcoes);
 
 		cOpcoesARemover = new ConjuntoMultiplo();
+		cOpcoesARemover.adicionarOpcao(o1);
 		cOpcoesARemover.adicionarOpcao(o2);
-		cOpcoesARemover.adicionarOpcao(o4);
 
-		conjuntoSolucao = new ConjuntoUnitario(o3);
+		conjuntoSolucao = new ConjuntoUnitario(o4);
 
 		q.setOpcoes(cOpcoes);
 		q.setOpcoesARemover(cOpcoesARemover);
@@ -79,12 +80,96 @@ public class JogoDummyDao implements JogoDataSource {
 
 		j.adicionarQuestao(q);
 
-		q = new Questao("Em O Senhor dos Anéis, quantos membros há inicialmente na Sociedade do Anel?", 10, 0);
+		q = new Questao("Qual é o nome do repórter conhecido por tomar choque na TV?", 10, 0);
 
-		o1 = new Opcao("8");
-		o2 = new Opcao("9");
-		o3 = new Opcao("10");
-		o4 = new Opcao("11");
+		o1 = new Opcao("Galvão");
+		o2 = new Opcao("Tino Marcos");
+		o3 = new Opcao("Lasier Martins");
+		o4 = new Opcao("Cid Moreira");
+
+		opcoes = new ArrayList<>();
+
+		opcoes.add(o1);
+		opcoes.add(o2);
+		opcoes.add(o3);
+		opcoes.add(o4);
+
+		cOpcoes = new ConjuntoOrdenado(opcoes);
+
+		cOpcoesARemover = new ConjuntoMultiplo();
+		cOpcoesARemover.adicionarOpcao(o1);
+		cOpcoesARemover.adicionarOpcao(o2);
+
+		conjuntoSolucao = new ConjuntoUnitario(o3);
+
+		q.setOpcoes(cOpcoes);
+		q.setOpcoesARemover(cOpcoesARemover);
+		q.setConjuntoSolucao(conjuntoSolucao);
+		
+		j.adicionarQuestao(q);
+		
+		q = new Questao("Quantos são os Vingadores do filme de 2012?", 10, 0);
+
+		o1 = new Opcao("5");
+		o2 = new Opcao("6");
+		o3 = new Opcao("7");
+		o4 = new Opcao("8");
+
+		opcoes = new ArrayList<>();
+
+		opcoes.add(o1);
+		opcoes.add(o2);
+		opcoes.add(o3);
+		opcoes.add(o4);
+
+		cOpcoes = new ConjuntoOrdenado(opcoes);
+
+		cOpcoesARemover = new ConjuntoMultiplo();
+		cOpcoesARemover.adicionarOpcao(o3);
+		cOpcoesARemover.adicionarOpcao(o4);
+
+		conjuntoSolucao = new ConjuntoUnitario(o2);
+
+		q.setOpcoes(cOpcoes);
+		q.setOpcoesARemover(cOpcoesARemover);
+		q.setConjuntoSolucao(conjuntoSolucao);
+		
+		j.adicionarQuestao(q);
+		
+		q = new Questao("Qual país não faz parte dos BRICS?", 10, 0);
+
+		o1 = new Opcao("África do Sul");
+		o2 = new Opcao("Canadá");
+		o3 = new Opcao("Brasil");
+		o4 = new Opcao("Rússia");
+
+		opcoes = new ArrayList<>();
+
+		opcoes.add(o1);
+		opcoes.add(o2);
+		opcoes.add(o3);
+		opcoes.add(o4);
+
+		cOpcoes = new ConjuntoOrdenado(opcoes);
+
+		cOpcoesARemover = new ConjuntoMultiplo();
+		cOpcoesARemover.adicionarOpcao(o3);
+		cOpcoesARemover.adicionarOpcao(o4);
+
+		conjuntoSolucao = new ConjuntoUnitario(o2);
+
+		q.setOpcoes(cOpcoes);
+		q.setOpcoesARemover(cOpcoesARemover);
+		q.setConjuntoSolucao(conjuntoSolucao);
+		
+		j.adicionarQuestao(q);
+		
+		q = new Questao("O que é \"De Morgan?\"", 10, 0);
+
+		o1 = new Opcao("Um ator");
+		o2 = new Opcao("Regra de Inferência");
+		o3 = new Opcao("Modelo de carro");
+		o4 = new Opcao("Regra de 3");
 
 		opcoes = new ArrayList<>();
 
@@ -100,6 +185,118 @@ public class JogoDummyDao implements JogoDataSource {
 		cOpcoesARemover.adicionarOpcao(o4);
 
 		conjuntoSolucao = new ConjuntoUnitario(o2);
+
+		q.setOpcoes(cOpcoes);
+		q.setOpcoesARemover(cOpcoesARemover);
+		q.setConjuntoSolucao(conjuntoSolucao);
+
+		j.adicionarQuestao(q);
+		
+		q = new Questao("Quantos ônibus Morgana contou na sua volta do Rio Sul?", 10, 0);
+
+		o1 = new Opcao("5 107 e 3 513");
+		o2 = new Opcao("6 107 e 3 513");
+		o3 = new Opcao("5 107 e 2 513");
+		o4 = new Opcao("6 107 e 2 513");
+
+		opcoes = new ArrayList<>();
+
+		opcoes.add(o1);
+		opcoes.add(o2);
+		opcoes.add(o3);
+		opcoes.add(o4);
+
+		cOpcoes = new ConjuntoOrdenado(opcoes);
+
+		cOpcoesARemover = new ConjuntoMultiplo();
+		cOpcoesARemover.adicionarOpcao(o1);
+		cOpcoesARemover.adicionarOpcao(o3);
+
+		conjuntoSolucao = new ConjuntoUnitario(o4);
+
+		q.setOpcoes(cOpcoes);
+		q.setOpcoesARemover(cOpcoesARemover);
+		q.setConjuntoSolucao(conjuntoSolucao);
+
+		j.adicionarQuestao(q);
+		
+		q = new Questao("Quantos professores estão na página do BSI?", 10, 0);
+
+		o1 = new Opcao("17");
+		o2 = new Opcao("29");
+		o3 = new Opcao("31");
+		o4 = new Opcao("38");
+
+		opcoes = new ArrayList<>();
+
+		opcoes.add(o1);
+		opcoes.add(o2);
+		opcoes.add(o3);
+		opcoes.add(o4);
+
+		cOpcoes = new ConjuntoOrdenado(opcoes);
+
+		cOpcoesARemover = new ConjuntoMultiplo();
+		cOpcoesARemover.adicionarOpcao(o1);
+		cOpcoesARemover.adicionarOpcao(o4);
+
+		conjuntoSolucao = new ConjuntoUnitario(o3);
+
+		q.setOpcoes(cOpcoes);
+		q.setOpcoesARemover(cOpcoesARemover);
+		q.setConjuntoSolucao(conjuntoSolucao);
+
+		j.adicionarQuestao(q);
+		
+		q = new Questao("Aham, senta lá...", 10, 0);
+
+		o1 = new Opcao("Claudia");
+		o2 = new Opcao("Bruna");
+		o3 = new Opcao("Ana");
+		o4 = new Opcao("Bia");
+
+		opcoes = new ArrayList<>();
+
+		opcoes.add(o1);
+		opcoes.add(o2);
+		opcoes.add(o3);
+		opcoes.add(o4);
+
+		cOpcoes = new ConjuntoOrdenado(opcoes);
+
+		cOpcoesARemover = new ConjuntoMultiplo();
+		cOpcoesARemover.adicionarOpcao(o2);
+		cOpcoesARemover.adicionarOpcao(o3);
+
+		conjuntoSolucao = new ConjuntoUnitario(o1);
+
+		q.setOpcoes(cOpcoes);
+		q.setOpcoesARemover(cOpcoesARemover);
+		q.setConjuntoSolucao(conjuntoSolucao);
+
+		j.adicionarQuestao(q);
+		
+		q = new Questao("Qual o recorde de dias sem treta no grupo geral de BSI?", 10, 0);
+
+		o1 = new Opcao("8");
+		o2 = new Opcao("15");
+		o3 = new Opcao("22");
+		o4 = new Opcao("26");
+
+		opcoes = new ArrayList<>();
+
+		opcoes.add(o1);
+		opcoes.add(o2);
+		opcoes.add(o3);
+		opcoes.add(o4);
+
+		cOpcoes = new ConjuntoOrdenado(opcoes);
+
+		cOpcoesARemover = new ConjuntoMultiplo();
+		cOpcoesARemover.adicionarOpcao(o1);
+		cOpcoesARemover.adicionarOpcao(o2);
+
+		conjuntoSolucao = new ConjuntoUnitario(o3);
 
 		q.setOpcoes(cOpcoes);
 		q.setOpcoesARemover(cOpcoesARemover);
